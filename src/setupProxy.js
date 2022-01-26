0237e22157d72487b74c3cc76d744a64916a9568
adhess <nicorobine@outlook.fr>
2022-01-26 14:31:56 +0100

    API_URL -> env variables

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://127.0.0.1:8080',
+            target: '${API_URL}',
             changeOrigin: true,
             pathRewrite: {
                 "^/api": "/"
